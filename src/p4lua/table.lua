940632cc1583e8bf721b7d0a1978e361e221df7d
Roy Jung <phi4grv@gmail.com>
2025-08-04 17:37:55 +1000

    Extract shared __newindex in p4lua.table.makeReadOnly

--- src/p4lua/table.lua
+++ src/p4lua/table.lua
@@ -1,5 +1,9 @@
 local pub = {}
 
+local readOnlyNewindex = function(_, key)
+    error("Attempt to modify readonly field '" .. tostring(key) .. "'")
+end
+
 pub.makeReadOnly = function(t)
     if type(t) ~= "table" then
         error("makeReadOnly expects a table, got " .. type(t))
@@ -7,9 +11,7 @@ pub.makeReadOnly = function(t)
 
     return setmetatable({}, {
         __index = t,
-        __newindex = function(_, key)
-            error("Attempt to modify readonly field '" .. tostring(key) .. "'")
-        end,
+        __newindex = readOnlyNewindex,
         __metatable = false,  -- Protect the metatable from being accessed or changed
     })
 end
