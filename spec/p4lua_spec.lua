8ce24bab5571a3be879da84ea5fd05378cc0996b
Roy Jung <phi4grv@gmail.com>
2025-08-08 03:06:22 +1000

    Fix incorrect test for p4lua.require

--- spec/p4lua_spec.lua
+++ spec/p4lua_spec.lua
@@ -1,21 +1,28 @@
 local assert = require("luassert")
+local p4fn = require("p4lua.fn")
 
-describe("p4lua.require", function()
+describe ("p4lua", function()
 
     local p4lua = require("p4lua")
-    local p4fn = require("p4lua.fn")
 
-    it("loads specified functions from module", function()
-        local f1, f2 = p4lua.require("p4lua.fn", { "compose", "compose_table" })
-        assert.are.same(f1, p4fn.compose)
-        assert.are.same(f2, p4fn.compose_table)
+    describe("p4lua.require", function()
+
+        it("loads specified functions from module", function()
+            local f1, f2 = p4lua.require("p4lua.fn", { "compose", "composeArray" })
+            assert.is_not_nil(f1)
+            assert.equal(f1, p4fn.compose)
+            assert.is_not_nil(f2)
+            assert.equal(f2, p4fn.composeArray)
+        end)
+
+        it("assign nil if function is not found", function()
+            local f1, f2, f3 = p4lua.require("p4lua.fn", { "compose", "not_exist", "composeArray" })
+            assert.is_not_nil(f1)
+            assert.equal(f1, p4fn.compose)
+            assert.is_nil(f2)
+            assert.is_not_nil(f3)
+            assert.equal(f3, p4fn.composeArray)
+        end)
+
     end)
-
-    it("assign nil if function is not found", function()
-        local f1, f2, f3 = p4lua.require("p4lua.fn", { "compose", "not_exist", "compose_table" })
-        assert.are.same(f1, p4fn.compose)
-        assert.is_nil(f2)
-        assert.are.same(f3, p4fn.compose_table)
-    end)
-
 end)
