81d6d09281d344a7a44ec0f96ad51612128017aa
Roy Jung <phi4grv@gmail.com>
2025-08-10 16:29:38 +1000

    Refactor p4lua.debug.print to point-free style

--- src/p4lua/debug.lua
+++ src/p4lua/debug.lua
@@ -1,16 +1,11 @@
 local inspect = require("inspect")
+local p4fn = require("p4lua.fn")
 local Array = require("p4lua.data.Array")
 
 local pub = {}
 
 pub.inspect = inspect
 
-pub.print = function(...)
-    local args = { ... }
-    local inspected = Array.fmap(pub.inspect, args)
-
-    ---@cast inspected string[]
-    print(table.unpack(inspected))
-end
+pub.print = p4fn.compose(print, table.unpack, Array.fmap(inspect), Array.fromVargs)
 
 return pub
